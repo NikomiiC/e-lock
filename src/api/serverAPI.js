c54eedc79caf012f41c14fbe58c095412eec9683
yw-ncs <139904407+yw-ncs@users.noreply.github.com>
2024-04-21 21:04:59 +0800

    Update serverAPI

--- src/api/serverAPI.js
+++ src/api/serverAPI.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const serverAPI = () => {
 
     const axiosInstance = axios.create({
-        baseURL: 'https://elockhub.onrender.com', //TODO: change later
+        baseURL: 'https://elock-server.onrender.com', //TODO: change later
         headers: {
             // 'Content-Type': contentType,
             "Access-Control-Allow-Origin": "*"
